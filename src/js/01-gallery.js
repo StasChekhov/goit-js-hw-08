53a34b55a3c68a13781b20486e107ed227c443dd
Stas Chekhov <7chekhov@gmail.com>
2021-11-26 16:39:21 +0200

    Fix bugs

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryUl = document.querySelector('.gallery');
+
+
+const galleryMarkup =  galleryItems.map((galleryItem) => {
+        return `
+        <a
+        class="gallery__item"
+        href="${galleryItem.original}"
+        >
+        <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"
+        />
+        </a>
+        `
+    })
+    .join('');
+
+
+galleryUl.insertAdjacentHTML('beforeend', galleryMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionSelector: 'img',
+  captionsData: 'alt',
+});
+
+console.log(galleryItems);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector(".feedback-form");
+const emailInputEL = document.querySelector("[name=email]");
+const textareaEL = document.querySelector("[name=message]");
+const USER_INFO = 'feedback-form-state';
+
+formEl.addEventListener("submit", onFormSubmit);
+emailInputEL.addEventListener("input", throttle(onEmailInput, 500));
+textareaEL.addEventListener("input", throttle(onTextareaInput, 500));
+
+let userInfo = {
+    email: '',
+    message: '',
+};
+
+getStorageInfo();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    console.log('userInfo :>> ', userInfo);
+    localStorage.removeItem(USER_INFO);
+}
+
+function onEmailInput(e) {
+    userInfo.email = e.target.value;
+    localStorage.setItem(USER_INFO, JSON.stringify(userInfo));
+}
+
+function onTextareaInput(e) {
+    userInfo.message = e.target.value;
+    localStorage.setItem(USER_INFO, JSON.stringify(userInfo));
+}
+
+function getStorageInfo() {
+    let savedInfo = localStorage.getItem(USER_INFO);
+
+    if (savedInfo) {
+        userInfo = JSON.parse(savedInfo);
+        emailInputEL.value = userInfo.email;
+        textareaEL.value = userInfo.message;
+    }
+}
